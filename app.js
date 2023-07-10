142be35b89704fa5d04c25887462df9a7f0bea5f
Isac <isac.mamede14@gmail.com>
2023-07-10 19:42:01 -0300

    working

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025']
+const colors = ['green', 'red', 'blue', 'blueviolet', 'brown', 'Rgba(133, 122, 200)', '#f15025', 'chartreuse', 'cyan']
 const btn = document.querySelector('#btn')
 const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    // get random number between 0-3
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+function getRandomNumber(){
+    return parseInt(Math.random()*colors.length) 
+}
