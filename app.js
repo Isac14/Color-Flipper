50ad144908e6842d05853c0a4b2fb142131251b4
Isac <isac.mamede14@gmail.com>
2023-07-10 21:30:50 -0300

    final changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const colors = ['green', 'red', 'blue', 'blueviolet', 'brown', 'Rgba(133, 122, 200)', '#f15025', 'chartreuse', 'cyan']
+const colors = ['green', 'red', 'blue', 'blueviolet', 'brown', 'chartreuse', 'cyan']
+const title = document.querySelector('#title')
 const btn = document.querySelector('#btn')
 const color = document.querySelector('.color')
 
 btn.addEventListener('click', function(){
     // get random number between 0-3
     const randomNumber = getRandomNumber()
-    console.log(randomNumber)
     document.body.style.backgroundColor = colors[randomNumber]
+    title.style.color = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
 
